63f0d7c7ac1f26a2cf68e9e70d1abdf7a979675c
jose <176546458+tornado10011@users.noreply.github.com>
2026-09-21 16:23:13 -0600

    nuevo

---
+++ a+b elevado a la 2 dividido entre 3.py
@@ -0,0 +1,9 @@
+import os
+os.system("cls")
+
+num1=int(input("digite el primer numero: "))
+num2=int(input("digite el segundo numero: "))
+
+operacion=(num1+num2)**2/3
+
+print(f"el resultado de su operacion seria igual a: {operacion}")
